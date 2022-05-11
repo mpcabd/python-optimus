ea78e9966ee5c3bfe13c56675087c2f771f95321
gazorby <gazorby@pm.me>
2022-05-11 18:08:52 +0200

    🐛 fix: use Path

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 from setuptools.extension import Extension
-
+from pathlib import Path
 
 com_args = ["-std=c99", "-O3", "-fopenmp", "-W"]
 link_args = ["-fopenmp"]
@@ -9,13 +9,13 @@ link_args = ["-fopenmp"]
 extensions = [
     Extension(
         "optimus_id.__init__",
-        ["optimus_id/__init__.py"],
+        [str(Path("optimus_id") / "__init__.py")],
         extra_compile_args=com_args,
         extra_link_args=link_args,
     ),
     Extension(
         "optimus_id.optimus",
-        ["optimus_id/optimus.pyx"],
+        [str(Path("optimus_id") / "optimus.pyx")],
         extra_compile_args=com_args,
         extra_link_args=link_args,
     ),
