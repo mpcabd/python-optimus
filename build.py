d3b0ef9b8b7e0081216530cb19009fc4c8d6330c
gazorby <gazorby@pm.me>
2022-05-11 18:47:46 +0200

    🐛 fix: don't cythonize __init__.py on windows

--- build.py
+++ build.py
@@ -3,16 +3,10 @@ from Cython.Distutils import build_ext
 from setuptools.extension import Extension
 from pathlib import Path
 
-com_args = ["-std=c99", "-O3", "-fopenmp", "-W"]
+com_args = ["-std=c99", "-O3", "-fopenmp"]
 link_args = ["-fopenmp"]
 
 extensions = [
-    Extension(
-        "optimus_id.__init__",
-        [str(Path("optimus_id") / "__init__.py")],
-        extra_compile_args=com_args,
-        extra_link_args=link_args,
-    ),
     Extension(
         "optimus_id.optimus",
         [str(Path("optimus_id") / "optimus.pyx")],
